1054109b40372e7dbefb480db6172e71b12f3d31
Copeland <ncopelan@jomc08368.dhcp.unc.edu>
2015-03-29 18:15:10 -0400

    sunday night

--- js/script.js
+++ js/script.js
@@ -40,7 +40,8 @@ $(document).ready(function(){
         });
     
     console.log(totalPopulation);
-    buildChart(); //finally builds chart -- needs to be inside function but outside loop so it won't try to write the chart 100 times, etc.
+    buildChart();
+    buildPiechart();//finally builds chart -- needs to be inside function but outside loop so it won't try to write the chart 100 times, etc.
     }
     
 });
@@ -49,98 +50,87 @@ $(document).ready(function(){
 function buildChart(xml){ //tells how to build chart, but need to add buildChart blah blah in document ready above
     
     var chart1 = new Highcharts.Chart({
-    colors: ["#DCE3E8"],
-    chart: {
-        renderTo: 'chart',
-        type: 'areaspline',
-        style: {
-            fontFamily:'"Ubuntu", sans-serif',
-            fontWeight:'300'}
-    },
-    title: {
-        text: 'MILLENNIALS VS. TOTAL POPULATION'
-    },
-    xAxis: {
-        categories:
-            ['0-10 years old', '11-20 years old', '21-30 years old', '31-40 years old', '41-50 years old', '51-60 years old', '61-70 years old', '71-80 years old', '81-90 years old', '91-100 years old'],
-        plotBands: [{ // visualize the weekend
-            from: .8,
-            to: 2.9,
-            color: 'rgba(202, 139, 121, .2)'
-        }]
-    },
-    yAxis: {
-        title: {
-            text: 'Population'
-        }
-    },
-    series: [{
-        name: 'Other',
-        data: totalPopulation
-    }]
-    
-     
-})};
-    
-/*$(function () {
-    $('#chart-two').highcharts({
+        colors: ['#8B9FBB, #DCE3E8'],
         chart: {
-            type: 'areaspline'
-        },
-        title: {
-            text: 'Average fruit consumption during one week'
+            renderTo: 'chart',
+            type: 'areaspline',
+            style: {
+                fontFamily:'"Ubuntu", sans-serif',
+                fontWeight:'300'
+            }
         },
         legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'top',
-            x: 150,
-            y: 100,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+            enabled: false
+        },
+        title: {
+            text: 'MILLENNIALS VS. TOTAL POPULATION (JUNE 2014)'
         },
         xAxis: {
-            categories: [
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday',
-                'Sunday'
-            ],
-            plotBands: [{ // visualize the weekend
-                from: 4.5,
-                to: 6.5,
-                color: 'rgba(68, 170, 213, .2)'
+            categories:
+                ['0-10 years old', '11-20 years old', '21-30 years old', '31-40 years old', '41-50 years old', '51-60 years old', '61-70 years old', '71-80 years old', '81-90 years old', '91-100 years old'],
+            plotBands: [{ 
+                from: .8,
+                to: 2.9,
+                color: 'rgba(202, 139, 121, .25)'
             }]
         },
         yAxis: {
             title: {
-                text: 'Fruit units'
-            }
-        },
-        tooltip: {
-            shared: true,
-            valueSuffix: ' units'
-        },
-        credits: {
-            enabled: false
-        },
-        plotOptions: {
-            areaspline: {
-                fillOpacity: 0.5
+                text: 'Population'
             }
         },
         series: [{
-            name: 'John',
-            data: [3, 4, 3, 5, 4, 10, 12]
-        }, {
-            name: 'Jane',
-            data: [1, 3, 4, 3, 3, 5, 4]
+            name: 'Other',
+            data: totalPopulation
+        }]
+         
+    })
+};
+    
+function buildPiechart() {
+    $('#race-chart').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Browser market shares at a specific website, 2014'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
         }]
     });
-});*/
+};
 
     
